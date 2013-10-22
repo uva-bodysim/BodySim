7d935bd3837854b9571c0cd319e197a0dd3e5010
MisterAbc <zl4ry@virginia.edu>
2013-10-22 01:38:16 -0400

    Finished up binding to vertex group code

--- code/bind_to_vertex_group.py
+++ code/bind_to_vertex_group.py
@@ -1,16 +1,51 @@
 """
 Script that binds a cube to a vertex group.
+
+The wire frame of the blender object this is working on must
+    have the name "model". The vertex groups must be of this
+    "model" object.
 """ 
 
 import bpy
 
-body_obj = bpy.data.objects['model']
-cube_obj = bpy.data.objects["Cube"]
 
-# Bind the cube to a vertex group
-cube_obj_const = cube_obj.constraints.new(type="CHILD_OF")
-cube_obj_const.target = body_obj
-cube_obj_const.subtarget = "Head"
+def list_vertex_group():
+    """Returns a list of names to vertex groups"""
+    return bpy.data.objects["model"].vertex_groups.keys()
 
-# Reset the cube's relative location.
-cube_obj.location = (0.0, 0.0, 0.0)
+def select_vertex_group(vg_name):
+    """Given a vertex group name, selects and displays it on the screen"""
+    bpy.ops.object.mode_set(mode="OBJECT")
+    obj = bpy.data.objects["model"]
+    bpy.context.scene.objects.active = obj
+    bpy.ops.object.mode_set(mode="EDIT")
+    bpy.ops.mesh.select_all(action="DESELECT")
+    bpy.ops.object.vertex_group_set_active(group=vg_name)
+    bpy.ops.object.vertex_group_select()
+    
+def cancel_selection():
+    """Go back to object mode after selection"""
+    bpy.ops.object.mode_set(mode="OBJECT")
+
+def bind_to_vertex_group(obj):
+    """ Binds the object passed in to the currently selected vertex group"""
+    vg = bpy.context.object.vertex_groups.active
+    body_obj = bpy.data.objects['model']
+
+    # Bind the obj to a vertex group
+    obj_const = obj.constraints.new(type="CHILD_OF")
+    obj_const.target = body_obj
+    obj_const.subtarget = vg.name
+
+    # Reset the cube's relative location.
+    cube_obj.location = (0.0, 0.0, 0.0)
+
+    cancel_selection()
+
+if __name__ == "__main__":
+    cube_obj = bpy.data.objects["Cube"]
+    body_obj = bpy.data.objects['model']
+    print("Vertex groups: ")
+    print(list_vertex_group())
+    select_vertex_group("Neck")
+    bind_to_vertex_group(cube_obj)
